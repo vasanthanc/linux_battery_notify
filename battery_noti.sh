5d50c68dacfed3534ae060d2c5368eaa966f4af0
Vasanthan Chandrasekaran <vasanthan93c@gmail.com>
2021-04-25 18:53:40 +0530

    image and sound issue fixedwq

--- battery_noti.sh
+++ battery_noti.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
 BATTINFO=`acpi -b`
 if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:30:00 ]] ; then
-    DISPLAY=:0.0 /usr/bin/notify-send -t 10000 -u critical -i "$(pwd)/low-battery-level.svg" "low battery" "$BATTINFO"
-    ffplay "$(pwd)/galaxy_low_battery.mp3" -nodisp -nostats -hide_banner -autoexit
+	DISPLAY=:0.0 /usr/bin/notify-send -t 10000 -u critical -i "$SCRIPTPATH/low-battery-level.svg" "low battery" "$BATTINFO" && /usr/bin/ffplay "$SCRIPTPATH/galaxy_low_battery.mp3" -nodisp -nostats -hide_banner -autoexit
 fi
