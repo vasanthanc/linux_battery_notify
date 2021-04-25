b8239e5bce250537b979a6303028cfda425876a0
Vasanthan Chandrasekaran <vasanthan93c@gmail.com>
2021-04-25 18:14:24 +0530

    added notification sound also.

--- battery_noti.sh
+++ battery_noti.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 BATTINFO=`acpi -b`
-if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 06:30:00 ]] ; then
+if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:30:00 ]] ; then
     DISPLAY=:0.0 /usr/bin/notify-send -t 10000 -u critical -i "$(pwd)/low-battery-level.svg" "low battery" "$BATTINFO"
+    ffplay "$(pwd)/galaxy_low_battery.mp3" -nodisp -nostats -hide_banner -autoexit
 fi
